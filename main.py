b2f6eb37dba7f3613e125191901fc7dd6cdc3fd3
sofia2160 <116231099+soso872@users.noreply.github.com>
2022-10-20 00:54:15 +0000

    nome!

--- main.py
+++ main.py
@@ -3,6 +3,18 @@
 #print()= comando de saida
 print("Alo mundo")
 
+#quando quiser guardar uma String(frase,nome,endereço)
+#  = atribuir valor 
+nome = "soso"
+#quando quiser guardar um numero inteiro
+idade = 19   
+
+#exibir o meu nome(que esta dentro da variavel nome)
+print(nome)
+print(idade)
+
+
+
 
 ''' 
 comentario em bloco
