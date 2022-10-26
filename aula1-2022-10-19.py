a811ed405cd304779143735e5c5ce9f2585d299c
soso <116231099+soso872@users.noreply.github.com>
2022-10-26 23:13:54 +0000

    comando de entrada input()

---
+++ aula1-2022-10-19.py
@@ -0,0 +1,27 @@
+# meu primeiro projeto python
+#
+#print()= comando de saida
+print("Alo mundo")
+
+#quando quiser guardar uma String(frase,nome,endereço)
+#  = atribuir valor 
+nome = "soso"
+#quando quiser guardar um numero inteiro
+idade = 19   
+
+#exibir o meu nome(que esta dentro da variavel nome)
+print(nome)
+ 
+#quando quiser exibir a frase "minha idade é"completando com conteudo da variavel idade
+#print("meu nome é " +nome)
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+#quando quiser exibir "meu nome é... e tenho...anos" trocando pelas variaveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome,idade))
+
+''' 
+comentario em bloco
+
+'''
--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-# meu primeiro projeto python
-#
-#print()= comando de saida
-print("Alo mundo")
+# comando input():quero permitir que o usuario digite algo
 
-#quando quiser guardar uma String(frase,nome,endereço)
-#  = atribuir valor 
-nome = "soso"
-#quando quiser guardar um numero inteiro
-idade = 19   
+nome = input("digite seu nome: ")
+#pede a idade para o usuario "qual sua idade?"
+idade = int(input("qual sua idade?: "))
 
-#exibir o meu nome(que esta dentro da variavel nome)
-print(nome)
- 
-#quando quiser exibir a frase "minha idade é"completando com conteudo da variavel idade
-#print("meu nome é " +nome)
-print("Minha idade é "+str(idade)+" anos")
-print(f"Minha idade é {idade}\n")
-print("Minha idade é {}".format(idade))
+#comando de saida...exibir na tela
+print(f"seu nome é {nome}")
 
-#quando quiser exibir "meu nome é... e tenho...anos" trocando pelas variaveis nome e idade
-print("Meu nome é {} e tenho {} anos".format(nome,idade))
+#exiba "sua idade é"
+print("sua idade é {}".format(idade))
 
-''' 
-comentario em bloco
-
-'''
+#se quiser mostrar o dobro da idade informada
+double=idade*2
+print("o dobro da idade informada é {}".format(double))
